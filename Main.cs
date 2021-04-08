aafc8e375c63fec6935eaaa267f8d0584e04de0d
uschwell <45977705+uschwell@users.noreply.github.com>
2021-04-08 18:05:01 +0200

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Client;
+
+namespace WpfApp1
+{
+    class Main
+    {
+
+
+        int x = 1;
+
+         //Console.WriteLine("Hello World!");
+
+
+
+        // Main Method
+        /*
+        static void Client(string[] args)
+        {
+            ExecuteClient();
+        }
+        */
+
+                // Main Method
+        /*
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+        */
+
+
+
+    }
+}
